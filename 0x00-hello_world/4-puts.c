2fc24b5dc09bd6e55b84c92787a467acffc135db
Nad0a <nadaatiki00@gmail.com>
2023-06-15 07:25:25 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+ - betty 4-puts.c ==> khass tkhrjlek bhal haka main 
+
+========== 4-puts.c ==========
+main
